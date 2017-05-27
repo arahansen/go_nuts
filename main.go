ec6189c47c1f6c3e82b861278803a39c744b6abe
Andrew Hansen <arahan@uw.edu>
2017-05-27 13:41:37 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"gopkg.in/kataras/iris.v6"
+	"gopkg.in/kataras/iris.v6/adaptors/httprouter"
+)
+
+type User struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Age       int    `json:"age"`
+}
+
+func main() {
+	app := iris.New()
+	app.Adapt(httprouter.New())
+
+	app.Post("/decode", func(ctx *iris.Context) {
+		var user User
+		ctx.ReadJSON(&user)
+
+		ctx.Writef("%s %s is %d years old!", user.Firstname, user.Lastname, user.Age)
+	})
+
+	app.Get("/encode", func(ctx *iris.Context) {
+		newUser := User{
+			Firstname: "John",
+			Lastname:  "Doe",
+			Age:       25,
+		}
+
+		ctx.JSON(iris.StatusOK, newUser)
+	})
+
+	app.Listen(":8080")
+}
